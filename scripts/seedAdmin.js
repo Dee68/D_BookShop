721b49db5d7077f1a0ba6dbd6b3519a1fa21c40a
dimie egberipou <ddimie283@gmail.com>
2026-05-12 20:29:08 +0100

    Correct seed admin script.

--- scripts/seedAdmin.js
+++ scripts/seedAdmin.js
@@ -21,45 +21,33 @@ const bcrypt = require("bcrypt");
 const pool = require("../backend/config/db");
 
 async function seedAdmin() {
-    try {
+    const existing = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [process.env.ADMIN_EMAIL]
+    );
 
-        const email = "admin@dbookshop.com";
-        const password = "Admin123!";
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // check if admin exists
-        const existing = await pool.query(
-            `SELECT * FROM users WHERE email = $1`,
-            [email]
-        );
-
-        if (existing.rows.length > 0) {
-            console.log("Admin already exists");
-            //process.exit();
-        }
-
-        await pool.query(
-            `
-            INSERT INTO users (name, email, password, role)
-            VALUES ($1, $2, $3, $4)
-            `,
-            [
-                "Admin",
-                email,
-                hashedPassword,
-                "admin"
-            ]
-        );
-
-        console.log("Admin user created successfully");
-
-        //process.exit();
-
-    } catch (err) {
-
-        console.error(err);
-        //process.exit(1);
+    if (existing.rows.length > 0) {
+        console.log("Admin already exists");
+        return;
     }
+
+    const hashedPassword = await bcrypt.hash(
+        process.env.ADMIN_PASSWORD,
+        10
+    );
+
+    await pool.query(
+        `INSERT INTO users (name, email, password, role)
+         VALUES ($1, $2, $3, $4)`,
+        [
+            "Admin",
+            process.env.ADMIN_EMAIL,
+            hashedPassword,
+            "admin"
+        ]
+    );
+
+    console.log("Admin user created successfully");
 }
 
 module.exports = seedAdmin;
