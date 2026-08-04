48d7efa92afce048d32ed267519a2e77f81b82fa
dimie egberipou <ddimie283@gmail.com>
2026-08-04 12:21:24 +0100

    Railway deployment changes

--- backend/server.js
+++ backend/server.js
@@ -17,30 +17,57 @@
 // app.listen(PORT, () => {
 //     console.log(`Server running on http://localhost:${PORT}`);
 // });
-require('./database/init')();
-const app = require('./app');
-const pool = require('./config/db'); // pg Pool
-//const seedAdmin = require("../scripts/seedAdmin");
-//const resetAdmin = require("../scripts/resetAdmin");
+// require('./database/init')();
+// const app = require('./app');
+// const pool = require('./config/db'); // pg Pool
+// //const seedAdmin = require("../scripts/seedAdmin");
+// //const resetAdmin = require("../scripts/resetAdmin");
 
+
+// async function startServer() {
+//     try {
+//         await pool.query('SELECT NOW()'); // test DB connection
+//         console.log("PostgreSQL connected successfully");
+
+//         //await resetAdmin();
+
+//        //await seedAdmin();
+
+//         const PORT = process.env.PORT || 5000;
+
+//         app.listen(PORT, () => {
+//             console.log(`Server running on http://localhost:${PORT}`);
+//         });
+
+//     } catch (err) {
+//         console.error("PostgreSQL connection failed:", err.message);
+//         process.exit(1);
+//     }
+// }
+
+// startServer();
+require('dotenv').config();
+
+const app = require('./app');
+const pool = require('./config/db');
+const initDb = require('./database/init');
 
 async function startServer() {
     try {
-        await pool.query('SELECT NOW()'); // test DB connection
+        await pool.query('SELECT NOW()');
         console.log("PostgreSQL connected successfully");
 
-        //await resetAdmin();
-
-       //await seedAdmin();
+        await initDb();
+        console.log("Database initialized successfully");
 
         const PORT = process.env.PORT || 5000;
 
         app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
+            console.log(`Server running on port ${PORT}`);
         });
 
     } catch (err) {
-        console.error("PostgreSQL connection failed:", err.message);
+        console.error("Startup error:", err.message);
         process.exit(1);
     }
 }
